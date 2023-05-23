01130b5921b84433fcb24cb1bec364f8e39c0452
MRensen <markrensen@hotmail.com>
2023-05-23 10:10:09 +0200

    seperate Bonus class with more instructions

---
+++ src/Bonus.java
@@ -0,0 +1,66 @@
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Bonus {
+
+    public static void main(String[] args) {
+
+        // Onderstaande code is uitgecommentarieerd. Haal de code uit de comment (delete /* en */) om aan de slag te gaan met de bonus
+        // In de code staan op verschillende plekken comments tussen "/*...*/" als hint wat je moet doen
+
+
+        HashSet<Integer> secretnumber = randomnumbergenerator();
+        String stringnumber = setToStringConverter(secretnumber);
+        System.out.println(stringnumber);
+        feedback(/*vul hier het juiste argument in*/);
+
+    }
+
+    public static void/*moet dit returntype "void" zijn of wat anders?*/ randomnumbergenerator(/*Heeft deze methode nog parameter(s) nodig?*/){
+         /*
+        Vul hier de body van de methode in.
+
+        Stappenplan:
+        - Maak een nieuwe variabele van type Random. (Tip: Zoek op internet hoe je Random kunt gebruiken)
+        - Maak een nieuwe variabele van type HashSet.
+        - Schrijf een while-loop om 4 random nummers aan de hashset toe te voegen
+        - return de hashset
+         */
+    }
+
+    public static void/*moet dit returntype "void" zijn of wat anders?*/ setToStringConverter(/*Heeft deze methode nog parameter(s) nodig?*/){
+        /*
+        Vul hier de body van de methode in.
+
+        Stappenplan:
+        - Maak als eerst een String variabele met als waarde een lege String. (of gebruik een StringBuilder)
+        - Schrijf vervolgens een for-loop om de items in de hashset een voor een aan de String variabele toe te voegen.
+        - Return de (gevulde) String variabele
+         */
+    }
+
+
+
+    public static void/*moet dit "void" zijn of wat anders?*/ feedback(String stringnumber) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder feedback = new StringBuilder();
+        System.out.println("+ = juiste nummer op de juiste plek, O = juiste nummer verkeerde plek, X = verkeerde nummer");
+        System.out.println("Doe een gok, Let op vul 4 getallen in.");
+        String guess = scanner.nextLine();
+        if (Objects.equals(guess, stringnumber)) {
+            System.out.println("gefeliciteerd je hebt het goed");
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
+                    feedback.append("+");
+                } else if (stringnumber.contains(guess.substring(i, i + 1))) {
+                    feedback.append("0");
+                } else {
+                    feedback.append("X");
+                }
+            }
+        }
+        System.out.println(feedback.toString());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,9 @@
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code scrijven voor de hoofd-opdracht
+        // Hier mag je je code schrijven voor de hoofd-opdracht
 
-        /* deze regel mag je weg halen voor de bonus opdracht. Onderstaande code is voor de bonus opdracht.
-        HashSet<Integer> secretnumber = randomnumbergenerator();
-        String stringnumber =  setToStringConverter(secretnumber);
-        System.out.println(stringnumber);
-        feedback();
-         deze regel mag je weg halen voor de bonus opdracht */
 
-    }
-
-    /*
-     Deze methode is voor de bonus opdracht.
-     */
-    public static void feedback(String stringnumber) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder feedback = new StringBuilder();
-        System.out.println("+ = juiste nummer op de juiste plek, O = juiste nummer verkeerde plek, X = verkeerde nummer");
-        System.out.println("Doe een gok, Let op vul 4 getallen in.");
-        String guess = scanner.nextLine();
-        if (Objects.equals(guess, stringnumber)) {
-            System.out.println("gefeliciteerd je hebt het goed");
-        } else {
-            for (int i = 0; i < 4; i++) {
-                if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
-                    feedback.append("+");
-                } else if (stringnumber.contains(guess.substring(i, i + 1))) {
-                    feedback.append("0");
-                } else {
-                    feedback.append("X");
-                }
-            }
-        }
-        System.out.println(feedback.toString());
     }
 }
